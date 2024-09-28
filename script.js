47d2d6452a6670215f5f0cc23fd60d05b39be1f1
Mahakprajapati <mahakprajapati227@gmail.com>
2024-09-28 20:54:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputFields = document.querySelectorAll("input");
+const unCompleted = document.querySelector(".uncompleted");
+const form = document.querySelector(".form");
+const button = document.querySelector("button");
+const Email_address =document.querySelector('.Email-address');
+
+// console.log(button);
+
+let filledInputFields = false;
+
+inputFields.forEach((input) => {
+    // console.log(input.value);
+
+   input.addEventListener("input", () => {
+    console.log(input.id);
+        
+        filledInputFields = [...inputFields].every((input) => {
+            //    console.log(input.value);
+            return input.value;
+        });
+        // console.log(filledInputFields);
+    });
+    input.addEventListener("focus", () => {
+        form.classList.remove("uncompleted");
+    });
+});
+
+button.addEventListener("click", (e) => {
+    // console.log("hello");
+    form.classList.add('placeholder');
+
+    if (!filledInputFields ) {
+        form.classList.add("uncompleted");
+    }
+    
+});
